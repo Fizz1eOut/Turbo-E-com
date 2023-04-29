e69b60831813b2f7b783d624b956a47fdc12a737
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-04-29 11:31:04 +0200

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,72 @@
+const langArr = {
+    "shop" :  {
+        "ru": "Магазин",
+        "en": "Shop",
+        "ua": "Магазин",
+    }, 
+    "collections": {
+        "ru": "Коллекции",
+        "en": "collections",
+        "ua": "Колекції",
+    }, 
+    "сandles": {
+        "ru": "Свечи",
+        "en": "Candles",
+        "ua": "Свічки",
+    }, 
+    "apiary": {
+        "ru": "Пасека",
+        "en": "Apiary",
+        "ua": "Пасіка",
+    }, 
+    "decor": {
+        "ru": "Декор",
+        "en": "Decor",
+        "ua": "Декор",
+    }, 
+    "gifts": {
+        "ru": "Подарочные боксы",
+        "en": "Gift boxes",
+        "ua": "Подарункові бокси",
+    }, 
+    "order": {
+        "ru": "Корпоративные заказы",
+        "en": "Corporate orders",
+        "ua": "Корпоративні замовлення",
+    }, 
+    "contacts": {
+        "ru": "Контакты",
+        "en": "Contacts",
+        "ua": "Контакти",
+    }, 
+    "account": {
+        "ru": "Аккаунт",
+        "en": "Account",
+        "ua": "Аккаунт",
+    }, 
+    "title1": {
+        "ru": "ТЕПЛЫЙ",
+        "en": "WARM",
+        "ua": "ТЕПЛИЙ",
+    }, 
+    "started-text": {
+        "ru": "СОЗДАЕМ УЮТ",
+        "en": "WE CREATE COZY",
+        "ua": "СТВОРЮЄМО ЗАТИШОК",
+    }, 
+    "title2": {
+        "ru": "ОГОНЕК",
+        "en": "FIRE",
+        "ua": "ВОГНИК",
+    }, 
+    "title3": {
+        "ru": "ЗАБОТЫ",
+        "en": "WORRIES",
+        "ua": "ТУРБОТИ",
+    }, 
+    "started-button": {
+        "ru": "в каталог",
+        "en": "to the catalog",
+        "ua": "до каталогу",
+    }, 
+}
