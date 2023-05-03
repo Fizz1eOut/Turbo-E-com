e62316d3119f879576a11779028f1c22971e27c5
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-05-03 13:01:21 +0200

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -69,4 +69,100 @@ const langArr = {
         "en": "to the catalog",
         "ua": "до каталогу",
     }, 
+
+    "row-text": {
+        "ru": "ДЛЯ ТВОЕГО УЮТА",
+        "en": "FOR YOUR COMFORT",
+        "ua": "ДЛЯ ТВОГО ЗАТИШКУ",
+    }, 
+    "row-title": {
+        "ru": "ЛЮБИМЫЕ СВЕЧИ",
+        "en": "FAVORITE CANDLES",
+        "ua": "УЛЮБЛЕНІ СВІЧКИ",
+    }, 
+    "row-button": {
+        "ru": "К КАТАЛОГУ",
+        "en": "to the catalog",
+        "ua": "ДО КАТАЛОГУ",
+    }, 
+    "product-button": {
+        "ru": "ДОБАВИТЬ В КОРЗИНУ",
+        "en": "ADD TO CART",
+        "ua": "ДОДАТИ ДО КОШИКА",
+    }, 
+    "product-button2": {
+        "ru": "ДОБАВИТЬ В КОРЗИНУ",
+        "en": "ADD TO CART",
+        "ua": "ДОДАТИ ДО КОШИКА",
+    }, 
+    "product-button3": {
+        "ru": "ДОБАВИТЬ В КОРЗИНУ",
+        "en": "ADD TO CART",
+        "ua": "ДОДАТИ ДО КОШИКА",
+    }, 
+    "product-button4": {
+        "ru": "ДОБАВИТЬ В КОРЗИНУ",
+        "en": "ADD TO CART",
+        "ua": "ДОДАТИ ДО КОШИКА",
+    }, 
+    "product-type": {
+        "ru": "СВЕЧИ СОЕВЫЕ",
+        "en": "SOY CANDLES",
+        "ua": "СВІЧКИ СОЄВІ",
+    },
+    "product-type2": {
+        "ru": "СВЕЧИ СОЕВЫЕ",
+        "en": "SOY CANDLES",
+        "ua": "СВІЧКИ СОЄВІ",
+    },
+    "product-type3": {
+        "ru": "СВЕЧИ СОЕВЫЕ",
+        "en": "SOY CANDLES",
+        "ua": "СВІЧКИ СОЄВІ",
+    },
+    "product-type4": {
+        "ru": "СВЕЧИ СОЕВЫЕ",
+        "en": "SOY CANDLES",
+        "ua": "СВІЧКИ СОЄВІ",
+    },
+    "product-title": {
+        "ru": "Дождь в Карпатах",
+        "en": "ain in the Carpathians",
+        "ua": "Дощ у Карпатах",
+    }, 
+    "product-title2": {
+        "ru": "Тыквенный пирог",
+        "en": "Pumpkin pie",
+        "ua": "Гарбузовий пиріг",
+    }, 
+    "product-title3": {
+        "ru": "Опавшие листья",
+        "en": "Fallen leaves",
+        "ua": "Опале листя",
+    }, 
+    "product-title4": {
+        "ru": "Сахарный каштан",
+        "en": "Sugar chestnut",
+        "ua": "Цукровий каштан",
+    }, 
+    "together": {
+        "ru": "Разом",
+        "en": "Together",
+        "ua": "Вместе",
+    }, 
+    "delivery": {
+        "ru": "Доставка",
+        "en": "Delivery",
+        "ua": "Доставка",
+    }, 
+    "np": {
+        "ru": "по тарифу ЧП",
+        "en": "according to the NP tariff",
+        "ua": "за тарифом НП",
+    }, 
+    "cart-button": {
+        "ru": "Оформить заказ",
+        "en": "To order",
+        "ua": "Оформити замовлення",
+    }, 
 }
