76f232423fc1c4142ceaf674e1f4ef859cac83e5
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-05-06 18:29:11 +0200

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -127,7 +127,7 @@ const langArr = {
     },
     "product-title": {
         "ru": "Дождь в Карпатах",
-        "en": "ain in the Carpathians",
+        "en": "Ain in the Carpathians",
         "ua": "Дощ у Карпатах",
     }, 
     "product-title2": {
@@ -165,4 +165,154 @@ const langArr = {
         "en": "To order",
         "ua": "Оформити замовлення",
     }, 
+
+    "bodal-title": {
+        "ru": "Оформление заказа",
+        "en": "Order processing",
+        "ua": "Оформлення заказу",
+    }, 
+    "modal-number": {
+        "ru": "Заказ",
+        "en": "Order",
+        "ua": "Замовлення",
+    }, 
+    /* 
+    "modal-quantity": {
+        "ru": "Количество",
+        "en": "Quantity",
+        "ua": "Кількість",
+    }, 
+    "modal-sum": {
+        "ru": "Вместе",
+        "en": "Together",
+        "ua": "Разом",
+    }, 
+    "modal-button": {
+        "ru": "Список желаний",
+        "en": "Wish list",
+        "ua": "Список бажань",
+    }, 
+    */
+    "order-name": {
+        "ru": "Имя",
+        "en": "Name",
+        "ua": "Ім’я",
+    }, 
+    "order-phone": {
+        "ru": "Номер телефона",
+        "en": "Phone number",
+        "ua": "Номер телефону",
+    }, 
+    "order-email": {
+        "ru": "Эмейл",
+        "en": "email",
+        "ua": "Емейл",
+    }, 
+    "order-btn": {
+        "ru": "Оформить заказ",
+        "en": "Make a request",
+        "ua": "Оформити замовлення",
+    }, 
+
+    "product-info-title": {
+        "ru": "Ароматы, действительно пахнущие при горении и бесплатная подарочная упаковка",
+        "en": "Fragrances that really smell when burned and free gift wrapping",
+        "ua": "Аромати, що дійсно пахнуть при горінні та безкоштовне подарункове упакування",
+    }, 
+    "product-info-text1": {
+        "ru": "В нашем ассортименте боксы большие, средние и малые. Только свечи, или комбинации с пряниками, тыквами, шоколадом или медом. С открытками, красной лентой и сертификатом.",
+        "en": "Our assortment includes large, medium and small boxes. Only candles, or combinations with gingerbread, pumpkins, chocolate or honey. With greeting cards, red ribbon and certificate.",
+        "ua": "У нашому асортименті бокси великі, середні та малі. Лише свічки, або ж комбінації з пряниками, гарбузиками, шоколадом чи медом. З вітальними листівками, червоною стрічкою та сертифікатом.",
+    }, 
+    "product-info-text2": {
+        "ru": "Ты можешь сам выбрать свой подарочный бокс или заменить уже добавленную вещь по своим предпочтениям, просто указав пожелания в комментарии к заказу",
+        "en": "You can choose your own gift box or replace the already added item according to your preferences, simply by specifying your wishes in the comments to the order",
+        "ua": "Ти можеш сам обрати свій подарунковий бокс або замінити вже додану річ за своїми уподобаннями, просто вказавши побажання у коментарі до замовлення",
+    }, 
+    "product-info-button": {
+        "ru": "К КАТАЛОГУ",
+        "en": "to the catalog",
+        "ua": "ДО КАТАЛОГУ",
+    }, 
+    "fragrance-title1": {
+        "ru": "ПОДБЕРИ",
+        "en": "PICK UP",
+        "ua": "ПІДБЕРИ",
+    }, 
+    "fragrance-text": {
+        "ru": "ПРОЙДИ тест на аромат",
+        "en": "PASS the aroma test",
+        "ua": "ПРОЙДИ тест на аромат",
+    }, 
+    "fragrance-title2": {
+        "ru": "СВОЙ",
+        "en": "YOUR",
+        "ua": "СВІЙ",
+    },
+    "fragrance-title3": {
+        "ru": "АРОМАТ",
+        "en": "AROMA",
+        "ua": "АРОМАТ",
+    },
+    "fragrance-alert": {
+        "ru": "Ответь на несколько вопросов и мы покажем какие ароматы больше всего подойдут именно тебе",
+        "en": "Answer a few questions and we will show you which fragrances are best for you",
+        "ua": "Дай відповідь на кілька запитань і ми покажемо які аромати найбільше підійдуть саме тобі",
+    },
+    "fragrance-btn": {
+        "ru": "Начать тест",
+        "en": "Start the test",
+        "ua": "Розпочати тест",
+    },
+
+    "fragrance-info-title": {
+        "ru": "Интересуют корпоративные подарки? Получи пробный бокс бесплатно!",
+        "en": "Interested in corporate gifts? Get a trial box for free!",
+        "ua": "Цікавлять корпоративні подарунки?Отримай пробний бокс безкоштовно!",
+    },
+    "fragrance-info-text1": {
+        "ru": "Мы бесплатно отправим бокс, ты сможешь оставить себе свечи или подарить их своим работникам.",
+        "en": "We will send the box for free, you can keep the candles for yourself or give them to your employees.",
+        "ua": "Ми безкоштовно відправимо бокс, ти зможеш залишити свічки собі чи подарувати їх своїм працівникам.",
+    },
+    "fragrance-info-text2": {
+        "ru": "Ты можешь сам выбрать свой подарочный бокс или заменить уже добавленную вещь по своим предпочтениям, просто указав пожелания в комментарии к заказу",
+        "en": "You can choose your own gift box or replace the already added item according to your preferences, simply by specifying your wishes in the comments to the order",
+        "ua": "Ти можеш сам обрати свій подарунковий бокс або замінити вже додану річ за своїми уподобаннями, просто вказавши побажання у коментарі до замовлення",
+    },
+    "fragrance-info-button": {
+        "ru": "Заказать бокс",
+        "en": "Order boxing",
+        "ua": "Замовити бокс",
+    },
+    "showrooms-title": {
+        "ru": "НАЙДИ НАС В ШОУРУМЕ, В ТВОЕМ ГОРОДЕ",
+        "en": "FIND US IN THE SHOWROOM IN YOUR CITY",
+        "ua": "ЗНАЙДИ НАС У ШОУРУМІ, У ТВОЄМУ МІСТІ",
+    },
+    "showrooms-address": {
+        "ru": "Адреса",
+        "en": "Addresses",
+        "ua": "Адреси",
+    },
+    "showrooms-text": {
+        "ru": "Наша продукция представлена ​​в 21 шоуруме",
+        "en": "Our products are presented in 21 showrooms",
+        "ua": "Наша продукція представлена у 21 шоурумі",
+    },
+    "showrooms-button": {
+        "ru": "Все города",
+        "en": "All cities",
+        "ua": "Всі міста",
+    },
+    "showrooms-text1": {
+        "ru": "Смотреть адреса в Киеве",
+        "en": "See addresses in Kyiv",
+        "ua": "Дивитись адреси у Києві",
+    },
+    "showrooms-text2": {
+        "ru": "Смотреть адреса во Львове",
+        "en": "See addresses in Lviv",
+        "ua": "Дивитись адреси у Львові",
+    },
 }
